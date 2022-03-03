cc5740c12d2c63cd83e82edb9f0ce78a99684557
Dawid930 <davidnyikes@gmail.com>
2022-03-03 15:10:05 +0100

    callback

--- frontend/script.js
+++ frontend/script.js
@@ -1,81 +1,40 @@
-function Country(name, short, population, flag, continent,) {
-    this.name = name;
-    this.short = short;
-    this.population = population;
-    this.flag = flag;
-    this.continent = continent;
+// immadeiatley invoked funtion  (function(){ })();
+//parameterk mindig function scope
+
+function init(mathFunction) {
+    var a = 5;
+    var b = 6;
+    
+
+    if (a < b){
+        let c = mathFunction(b, a)
+        console.log(a);
+        /* (function(){
+            var c = b - a;
+            console.log(c);
+        })(); */
+        
+        
+    } else {
+        let c = mathFunction(a, b)
+        console.log(c);
+        /* (function(){
+            var c = a - b;
+            console.log(c);
+        })(); */
+    
+    };
+
+    //console.log(c);
 }
 
-const menuButton = () => {
-    return`
-    <button id="menuButton">
-        <svg width="40" height="40">
-            <rect width="20" height="2"/>
-            <rect width="20" height="2"/>
-            <rect width="20" height="2"/>
-        </svg>
-        <span>button</span>
-    </button>
-    `;
+const initC = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
 };
 
-//Components
-const header = (logo, button) => {
-    return `
-    <header>
-        <a id="logo">${logo}</a>
-        ${button()}
-    </header>
-    `
-}
+const initD = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber
+};
 
 
-const countryCard = (name, short, population, flag, continent) => {
-    return`
-    <div class="card">
-        <h3>${name}</h3>
-        <h3>${short}</h3>
-        <h3>${population}</h3>
-        <h3>${continent}</h3>
-        <img src="${flag}"></img>
-    </div>
-    `
-}
-
-
-
-const loadEvent = async _ => {
-
-
-    // Get data
-    const countryRes = await fetch("https://restcountries.com/v3.1/all"); //await asincron muvelet, azert kell h megvarjuk a betoltest
-    const countryArr = await countryRes.json(); //counrtarr varja meg amig lefut a curtyres
-    //console.log(countryArr);
-
-
-
-    //Process data
-    let countries = countryArr.map(function(country){               //a map egy arrayt csinal!!!
-        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0])
-    })
-
-    console.log(countries);
-
-                // a country cardnak itt adjuk meg az ertekeket
-    let cards = "";
-    for (const country of countries){
-        cards += countryCard(country.name, country.short, country.population, country.flag, country.continent)
-    }
-
-    const rootElement = document.getElementById("root")
-    rootElement.insertAdjacentHTML("beforeend", header("Countries", menuButton));
-    rootElement.insertAdjacentHTML("beforeend", cards);
-
-
-    const getMenuButton = document.getElementById("menuButton")
-    getMenuButton.addEventListener("click", (event) => {
-        event.target.classList.toggle("clicked");
-    })
-}
-
-window.addEventListener("load", loadEvent)
+init(initD);
