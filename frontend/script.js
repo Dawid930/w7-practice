424705a5d9c455f0b7d18e43fb5beaa90f370286
Dawid930 <davidnyikes@gmail.com>
2022-03-03 14:38:17 +0100

    accidental

--- frontend/script.js
+++ frontend/script.js
@@ -45,19 +45,23 @@ const countryCard = (name, short, population, flag, continent) => {
 
 
 const loadEvent = async _ => {
+
+
     // Get data
     const countryRes = await fetch("https://restcountries.com/v3.1/all"); //await asincron muvelet, azert kell h megvarjuk a betoltest
     const countryArr = await countryRes.json(); //counrtarr varja meg amig lefut a curtyres
     //console.log(countryArr);
 
+
+
     //Process data
-    let countries = countryArr.map(function(country){ //a map egy arrayt csinal
+    let countries = countryArr.map(function(country){               //a map egy arrayt csinal!!!
         return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0])
     })
 
     console.log(countries);
 
-
+                // a country cardnak itt adjuk meg az ertekeket
     let cards = "";
     for (const country of countries){
         cards += countryCard(country.name, country.short, country.population, country.flag, country.continent)
@@ -66,6 +70,7 @@ const loadEvent = async _ => {
     const rootElement = document.getElementById("root")
     rootElement.insertAdjacentHTML("beforeend", header("Countries", menuButton));
     rootElement.insertAdjacentHTML("beforeend", cards);
+
 
     const getMenuButton = document.getElementById("menuButton")
     getMenuButton.addEventListener("click", (event) => {
