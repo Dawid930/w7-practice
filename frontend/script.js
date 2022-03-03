b0f527bf36297e61f62260f81e0adf6a1c4d708c
Dawid930 <davidnyikes@gmail.com>
2022-03-03 17:15:41 +0100

    form

--- frontend/script.js
+++ frontend/script.js
@@ -1,40 +1,80 @@
-// immadeiatley invoked funtion  (function(){ })();
-//parameterk mindig function scope
 
-function init(mathFunction) {
-    var a = 5;
-    var b = 6;
-    
 
-    if (a < b){
-        let c = mathFunction(b, a)
-        console.log(a);
-        /* (function(){
-            var c = b - a;
-            console.log(c);
-        })(); */
-        
-        
-    } else {
-        let c = mathFunction(a, b)
-        console.log(c);
-        /* (function(){
-            var c = a - b;
-            console.log(c);
-        })(); */
-    
-    };
 
-    //console.log(c);
+//itt a send button egy submit esemeny
+
+const formHTML = () => {
+    return`
+    <form>
+        <input placeholder="Name" id="input1" name="input1"/><br>
+        <input placeholder="Name" id="input2" name="input2"/><br>
+        <input placeholder="Name" id="input3" name="input3"/><br>
+        <select name="animals" id="animals">
+            <option value="cats555">cats</option>
+            <option value="dogs555">dogs</option>
+            <option value="both555">both</option>
+        </select>
+        <p id="nameResult"></p><br>
+        <button>Send</button>
+    </form>
+    `    
+};
+
+// input elonye minden platfronnmon normlisan fut, change nem fut le mindenhol 
+
+const loadEvent = _ => {
+    const rootElement = document.getElementById("root")
+    rootElement.insertAdjacentHTML("beforeend", formHTML())
+
+    const from = rootElement.querySelector("form")
+
+    const inputList = document.querySelectorAll("input") //arrayt csinal (node list, ezert plusz lepcso kell a map-nel, ==> Array.from,,, ez atalakitja a node listet array-e)
+
+    /*          // igy kell for ciklussal
+
+    for (const input of inputList) {
+        input.addEventListener("input", function(event){
+            console.log(event.target.value);
+        })   //inputra tessuk az esemenyfigyelot,  e=event
+    } */
+
+
+    // igy kell map-el   map-nek array kell, map arrayt ad vizza a callback functionbol ez ayert jo mert nem kell nekukn arrayt csinalni mert megcsinalja helyettunk, fenntarthato szintaktikat ad, kenyelmesebb,atekinthetobb lesz a kesobbiekben 
+
+    console.log(typeof inputList);
+    console.log(Array.isArray(inputList));
+    Array.from(inputList).map(function(input){
+        input.addEventListener("input", function(event){
+            console.log(event.target.value);
+        })
+    })
+/* 
+    rootElement.querySelector("form").addEventListener("submit", function(event){
+        event.preventDefault()
+        console.log(event.target);
+    }) */
+
+    form.querySelector("select").addEventListener("input", function(event){
+        console.log(event.target.value);
+    })
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault()
+        console.log(event.target);
+    })
+
+
+    /* const nameInput = document.getElementById("namePlace")
+    const log = document.getElementById("nameResult")
+
+    nameInput.addEventListener("input", function(e) {
+        log.textContent = e.target.value
+    }) */
+
+
+
+
 }
 
-const initC = (firstNumber, secondNumber) => {
-    return firstNumber - secondNumber;
-};
+window.addEventListener("load", loadEvent)
 
-const initD = (firstNumber, secondNumber) => {
-    return firstNumber * secondNumber
-};
-
-
-init(initD);
